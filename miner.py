9d0c4ad115062dda065958a39143f994a6c50da4
zhiwei-w-luo <zhiwei.w.luo@gmail.com>
2023-11-12 11:18:59 +0800

    add debug code

--- miner.py
+++ miner.py
@@ -98,25 +98,23 @@ else:
     print("The address is invalid. Correct your account address and try again")
     exit(0)
 
-# Dynamic get Account
 def get_current_account():
     """
     Determines the current account to which mining rewards should be submitted.
-    If DEVELOPER_MODE is enabled and the current time is within the first 20%
-    of any given hour, it returns the DEVELOPER_ACCOUNT. Otherwise, it returns
-    the account.
+    In developer mode, it returns the DEVELOPER_ACCOUNT for a specified fraction of each hour.
+    Otherwise, it returns the user's account.
     """
-    # In every hour, for 20% of the time, returns the developer's account, otherwise the user's account
     if DEVELOPER_MODE:
         current_minute = time.localtime().tm_min
         current_second = time.localtime().tm_sec
-        # Calculate the total seconds elapsed in the current hour
         total_seconds = current_minute * 60 + current_second
-        # Determine if we are in the developer's time slice (first 20% of an hour)
         if total_seconds < (3600 * DEVELOPER_TIME_FRACTION):
+            print(f"Developer mode active: total_seconds = {total_seconds}, using DEVELOPER_ACCOUNT")
             return DEVELOPER_ACCOUNT
-    # Default to the user's account
-    return eth_address
+        else:
+            print(f"User mode active: total_seconds = {total_seconds}, using USER_ACCOUNT")
+            return account
+    return account
 
 
 # Access other settings
